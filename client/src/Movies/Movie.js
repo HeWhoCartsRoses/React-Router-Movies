82dbfc308ca50cdeb3fc6b9216400de7fd3c0428
HeWhoCartsRoses <carteradrewrose@gmail.com>
2020-01-29 14:07:04 -0700

    finished

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,35 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const Movie = (props) => {
+  console.log('i got here boss!')
   const [movie, setMovie] = useState();
- 
+  console.log(props)
+  const { id } = useParams();
   useEffect(() => {
-    const id = 1;
+
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then(response => {
+        setMovie(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
 
-  },[]);
-  
+  }, [id]);
+
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
   //   addToSavedList(movie)
   // }
 
+
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
-
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
